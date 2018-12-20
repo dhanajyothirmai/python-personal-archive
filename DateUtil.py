e974b7532b5b6ff0973da1aa7031f01b2f855dfe
dhanajyothirmai <dhanajyothirmai415@gmail.com>
2018-12-20 11:49:18 +0530

    DateUtil.py file added

---
+++ DateUtil.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# the above line is to avoid 'SyntaxError: Non-UTF-8 code starting with' error
+
+'''
+Created on 
+
+@author: raja.raman
+
+source:
+    https://stackoverflow.com/questions/370357/python-variable-scope-error
+    https://stackoverflow.com/questions/10851906/python-3-unboundlocalerror-local-variable-referenced-before-assignment
+'''
+
+
+def check_date_between():
+    '''
+        DateUtilSGW::checkDateBetween
+    '''
+    pass
+
+def check_date_between_str():
+    '''
+        DateUtilSGW::checkDateBetween
+    '''
+    pass
+
+def compare_two_dates():
+    '''
+        DateUtilSGW::compareTwoDates
+    '''
+    pass
+
+def compare_two_string_dates():
+    '''
+        DateUtilSGW::compareTwoStringDates
+    '''
+    pass
+
+def getDate():
+    pass
+
+def getAge():
+    pass
+
+def getPreviousDateOfMonth():
+    pass
+
+def getNDaysBefore(n):
+    pass
+
+
+def main():
+    pass
+    
+    
+if __name__ == '__main__':
+    main()
